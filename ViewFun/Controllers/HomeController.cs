6eaa84b0e32cde868f732952775858fa26c8f0d0
Sean Gillespie <sean.gillespie22@gmail.com>
2019-08-10 17:01:16 -0700

    Coding Dojo View Fun

---
+++ ViewFun/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ViewFun.Models;
+
+namespace ViewFun.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            Person somePerson = new Person()
+            {
+                FirstName = "Bobs And",
+                LastName = "Floats"
+            };
+            return View(somePerson);
+        }
+    }
+}
---
+++ ViewFun/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ViewFun.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
